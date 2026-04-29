6a5f5153f69d287369105d2cdb03f4e7686a8806
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-04-30 02:38:23 +0530

    chor: remove unused import

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -2,14 +2,15 @@ const express = require("express");
 const transactionRouter = express.Router();
 
 const { userAuth } = require("../middleware/userAuth");
+
 const { addTransaction,
     getTransactions,
-    getTransactionById } = require("../controllers/transactionController");
-
-const authRouter = require("./authRoutes");
+    getTransactionById,
+} = require("../controllers/transactionController");
 
 transactionRouter.post("/", userAuth, addTransaction);
 transactionRouter.get("/", userAuth, getTransactions);
 transactionRouter.get("/:transactionId", userAuth, getTransactionById);
+
 
 module.exports = transactionRouter;
