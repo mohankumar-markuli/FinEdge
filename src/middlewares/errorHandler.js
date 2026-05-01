333a0f540ff9189e870491b09619fe02ce706401
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-05-02 01:49:15 +0530

    fix: add export errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, req, res, next) => {
-    console.log(err);
-
     res.status(err.statusCode || err.status || 500).json({
         message: err.message || "Internal Server Error",
     });
 };
+
+module.exports = { errorHandler };
