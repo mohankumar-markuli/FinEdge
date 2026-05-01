938c8631f3342046061ffb1eab23c2db38b746b1
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-05-02 01:46:50 +0530

    feat: include custom error codes

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,7 @@
 const errorHandler = (err, req, res, next) => {
     console.log(err);
-    res.status(err.status || 500).json({
+
+    res.status(err.statusCode || err.status || 500).json({
         message: err.message || "Internal Server Error",
     });
 };
-
-module.exports = { errorHandler };
